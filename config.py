354d330441d3ea5c803c695a5f0bd3bc483297a9
jason-rey <jasonrey@hotmail.com>
2023-11-05 02:19:42 -0800

    changed port in config to use the provided PORT env variable

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config():
     HOST_IP = os.getenv("host_ip")
-    HOST_PORT = os.getenv("host_port")
+    HOST_PORT = os.getenv("PORT")
 
     AUTH_SERVER_IP = os.getenv("auth_server_ip")
